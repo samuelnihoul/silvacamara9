e7c14acd2e725c765918b660d7a995d087d73b8a
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2024-02-29 20:59:18 +0100

    rm vuforia

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -7,18 +7,13 @@ public class Shooter : MonoBehaviour
     public float shootForce = 7000.0f;
     public int shootSpeed = 100;
     RaycastHit hit;
-    // Start is called before the first frame update
-    private int counter = 0;
-    // Update is called once per frame
     void Update()
     {
-        counter++;
-        if (counter == shootSpeed)
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject bullet = Instantiate(projectile, arCamera.position, arCamera.rotation) as GameObject;
             bullet.GetComponent<Rigidbody>().AddForce(arCamera.forward * shootForce);
             Debug.Log("shoot");
-            counter = 0;
         }
 
     }
